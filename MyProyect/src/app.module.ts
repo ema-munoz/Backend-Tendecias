ff9de3d895afd186e22acf044b48ad75ca041b6e
ema-munoz <ema.munoz@yavirac.edu.ec>
2022-06-16 09:41:40 -0500

    Cambios 16/06/2022 ??

--- MyProyect/src/app.module.ts
+++ MyProyect/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Barbarita19@2021@',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
